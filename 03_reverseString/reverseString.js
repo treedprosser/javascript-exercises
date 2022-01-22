ac742845c6068c9a10ca1acb44c5414d35d8ba5d
Trevor Prosser <treedprosser@gmail.com>
2022-01-22 09:10:26 -0700

    Add the correct function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
 const reverseString = function(str) {
-
+    let stringLetters = str.split("");
+    let reverseLetters = stringLetters.reverse();
+    return reverseLetters.join("");
 };
 
 // Do not edit below this line
