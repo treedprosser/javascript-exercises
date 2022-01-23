e6749b56920d8ee478ea0e7573bb44140498e90a
Trevor Prosser <treedprosser@gmail.com>
2022-01-23 15:44:33 -0700

    Add correct function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    for (var i = arr.length - 1; i >= 0; i--) {
+        for (var j = 0; j < toRemove.length; j++) {
+            if (arr[i] === toRemove[j]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
