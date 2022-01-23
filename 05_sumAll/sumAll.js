7003c6b329bd49b0a02b672454859c141e507a64
Trevor Prosser <treedprosser@gmail.com>
2022-01-23 16:11:33 -0700

    Add correct function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR'
+    } else {
+        if (num1 > 0 && num2 > 0) {
+            if (num1 <= num2) {
+                for (let i = num1; i <= num2; i++) {
+                    sum = sum + i;
+                }
+            } else {
+                for (let i = num2; i <= num1; i++) {
+                    sum = sum + i;
+                }
+            }
+        } else {
+            return 'ERROR';
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
