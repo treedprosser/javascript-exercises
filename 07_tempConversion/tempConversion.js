696cb7597068a75515d2d520d6ec29717c6407b1
Trevor Prosser <treedprosser@gmail.com>
2022-01-23 17:30:46 -0700

    Add correct functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let tempC = (temp - 32) * (5/9);
+  tempC = Math.round(tempC * 10) / 10;
+  return tempC;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let tempF = temp * (9/5) + 32;
+  tempF = Math.round(tempF * 10) / 10;
+  return tempF;
 };
 
 // Do not edit below this line
