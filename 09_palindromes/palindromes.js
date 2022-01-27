d53d1a822a3fbe2d10d4f3bb51ee5a562d63239b
Trevor Prosser <treedprosser@gmail.com>
2022-01-27 16:38:04 -0700

    Add correct function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/\W/g, '').toLowerCase();
+    let revString = string.split('').reverse().join('');
+    if (revString == string) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
