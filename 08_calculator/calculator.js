60d4907090a1469eb54b925c1ced339fe3ba9ce9
Trevor Prosser <treedprosser@gmail.com>
2022-01-26 22:17:27 -0700

    Add correct functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for (let i = 0; i < arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  if (num === 0) {
+    return 1;
+  } else {
+    for (let i = num; i > 0; i--) {
+      total *= i;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
