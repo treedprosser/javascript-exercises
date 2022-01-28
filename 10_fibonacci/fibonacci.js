892be701953b31bb7019e3e1906ed813257f737a
Trevor Prosser <treedprosser@gmail.com>
2022-01-27 17:52:45 -0700

    Add correct function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fib = [];
+    fib[0] = 0;
+    fib[1] = 1;
+    if (num < 0) {
+        return 'OOPS';
+    }
+    for (let i = 2; i <= num + 1; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[num];
 };
 
 // Do not edit below this line
